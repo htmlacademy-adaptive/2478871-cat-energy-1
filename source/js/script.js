53e17cf205f3aa99a278f64f1d0a0344306d283d
Regina <regina.m.shakurova@gmail.com>
2024-06-25 20:52:18 +0400

    добавление планшета

---
+++ source/js/script.js
@@ -0,0 +1,9 @@
+const nav = document.querySelector('.nav');
+const navToggle = document.querySelector('.nav__toggle');
+
+nav.classList.add('nav--closed');
+
+navToggle.addEventListener('click', () => {
+nav.classList.toggle('nav--closed');
+nav.classList.toggle('nav--opened');
+});
